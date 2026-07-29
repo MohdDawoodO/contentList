02725219be57e9ccb6413b1974c3171a9f232911
Serious_17 <serious_17O@outlook.com>
2026-07-29 14:41:47 +0530

    1. Added landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,90 @@
-import Image from "next/image";
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [currentID, setCurrentID] = useState(0);
+
+  const titles = [
+    {
+      title: "Game",
+      content: "games",
+      accent: "0ebb47",
+      bg: "0b0e0c",
+      link: "/game",
+    },
+    {
+      title: "Anime",
+      content: "anime",
+      accent: "d40003",
+      bg: "0d0b0b",
+      link: "/anime",
+    },
+    {
+      title: "Movie",
+      content: "movies",
+      accent: "1a6dda",
+      bg: "0b0d0e",
+      link: "/movie",
+    },
+  ];
+
+  const content = [
+    `Content List is a personal website created by Mohd Dawood to organise and keep track of his ${titles[currentID].content}.`,
+    "Although it's built around Mohd Dawood's collection, anyone can customise it by replacing the hardcoded data with their own.",
+    "Click the buttons below to see what Mohd Dawood is up to.",
+  ];
+
+  setInterval(() => {
+    setCurrentID((currentID + 1) % 3);
+  }, 5000);
+
+  useEffect(() => {}, []);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main
+      className="min-h-dvh duration-1000 transition-colors flex justify-center items-center px-4 sm:px-8"
+      style={{ background: "#" + titles[currentID].bg }}
+    >
+      <div className="flex flex-col justify-center items-center gap-8 max-w-2xl">
+        <div className="flex text-3xl font-semibold overflow-hidden">
+          {titles.map((title, i) => {
+            if (currentID !== i) return;
+            return (
+              <h1
+                key={title.title}
+                style={{
+                  color: "#" + title.accent,
+                }}
+                className={`w-24 text-end animate-slide`}
+              >
+                {title.title}
+              </h1>
+            );
+          })}
+
+          <h1>List</h1>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="flex flex-col text-center text-lg text-[#BDBDBD] gap-4 sm:text-xl">
+          {content.map((p) => (
+            <p key={p}>{p}</p>
+          ))}
         </div>
-      </main>
-    </div>
+
+        <div className="flex gap-4 sm:gap-6">
+          {titles.map((title) => (
+            <button
+              key={title.title}
+              className="p-2 px-4 w-22 rounded-md cursor-pointer hover:translate-y-px active:translate-y-1 duration-100 font-semibold"
+              style={{ background: "#" + title.accent }}
+            >
+              <Link href={title.link}>{title.title}</Link>
+            </button>
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }
