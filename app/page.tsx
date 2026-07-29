f467976883635685aca197dbefdc43cbe0d5490a
Serious_17 <serious_17O@outlook.com>
2026-07-29 21:04:32 +0530

    2. Improved landing page
    - added background images to landing page
    - improved animation logic of the title using motion library
    - minor polishing
    - renamed some variables for better understandability

--- app/page.tsx
+++ app/page.tsx
@@ -2,85 +2,131 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import gamesBG from "@/assets/backgrounds/games-bg.jpg";
+import animeBG from "@/assets/backgrounds/anime-bg.jpg";
+import moviesBG from "@/assets/backgrounds/movies-bg.jpg";
+import Image from "next/image";
 
 export default function Home() {
   const [currentID, setCurrentID] = useState(0);
 
-  const titles = [
+  const content = [
     {
       title: "Game",
-      content: "games",
+      body: "games",
       accent: "0ebb47",
       bg: "0b0e0c",
       link: "/game",
+      image: gamesBG,
     },
     {
       title: "Anime",
-      content: "anime",
+      body: "anime",
       accent: "d40003",
       bg: "0d0b0b",
       link: "/anime",
+      image: animeBG,
     },
     {
       title: "Movie",
-      content: "movies",
+      body: "movies",
       accent: "1a6dda",
       bg: "0b0d0e",
       link: "/movie",
+      image: moviesBG,
     },
   ];
 
-  const content = [
-    `Content List is a personal website created by Mohd Dawood to organise and keep track of his ${titles[currentID].content}.`,
+  const paragraphs = [
+    `Content List is a personal website created by Mohd Dawood to organise and keep track of his ${content[currentID].body}.`,
     "Although it's built around Mohd Dawood's collection, anyone can customise it by replacing the hardcoded data with their own.",
     "Click the buttons below to see what Mohd Dawood is up to.",
   ];
 
-  setInterval(() => {
-    setCurrentID((currentID + 1) % 3);
-  }, 5000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentID((currentID + 1) % 3);
+    }, 8000);
 
-  useEffect(() => {}, []);
+    return () => clearInterval(interval);
+  }, [currentID]);
+
+  const MotionImage = motion.create(Image);
 
   return (
-    <main
-      className="min-h-dvh duration-1000 transition-colors flex justify-center items-center px-4 sm:px-8"
-      style={{ background: "#" + titles[currentID].bg }}
-    >
-      <div className="flex flex-col justify-center items-center gap-8 max-w-2xl">
-        <div className="flex text-3xl font-semibold overflow-hidden">
-          {titles.map((title, i) => {
-            if (currentID !== i) return;
-            return (
-              <h1
-                key={title.title}
-                style={{
-                  color: "#" + title.accent,
-                }}
-                className={`w-24 text-end animate-slide`}
-              >
-                {title.title}
-              </h1>
-            );
-          })}
+    <main className="min-h-dvh flex justify-center items-center px-4 sm:px-8">
+      <AnimatePresence mode="wait">
+        {content.map((item, i) => {
+          if (currentID !== i) return;
+          return (
+            <motion.div
+              key={item.title}
+              className="gradient w-full h-full absolute before:w-full before:h-full before:backdrop-blur-xs before:absolute before:z-1"
+              style={{
+                backgroundImage: `linear-gradient(#${item.bg}bf, #${item.bg}fc 80%)`,
+              }}
+            >
+              <MotionImage
+                className="absolute w-full h-full object-cover object-top -z-1 pointer-events-none"
+                src={item.image}
+                alt="ss"
+                width={1920}
+                height={1080}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
+
+      <div className="flex flex-col justify-center items-center gap-8 max-w-2xl z-10">
+        <div className="flex text-3xl font-semibold overflow-hidden relative">
+          <AnimatePresence mode="popLayout">
+            {content.map((item, i) => {
+              if (currentID !== i) return;
+              return (
+                <motion.h1
+                  key={item.title}
+                  style={{
+                    color: "#" + item.accent,
+                    textShadow: `0px 0px 10px #${item.accent}bf`,
+                  }}
+                  className={`w-24 text-end`}
+                  initial={{ y: "-100%" }}
+                  animate={{ y: 0 }}
+                  exit={{ y: "100%" }}
+                  transition={{ duration: 1 }}
+                >
+                  {item.title}
+                </motion.h1>
+              );
+            })}
+          </AnimatePresence>
 
           <h1>List</h1>
         </div>
 
         <div className="flex flex-col text-center text-lg text-[#BDBDBD] gap-4 sm:text-xl">
-          {content.map((p) => (
+          {paragraphs.map((p) => (
             <p key={p}>{p}</p>
           ))}
         </div>
 
         <div className="flex gap-4 sm:gap-6">
-          {titles.map((title) => (
+          {content.map((item) => (
             <button
-              key={title.title}
+              key={item.title}
               className="p-2 px-4 w-22 rounded-md cursor-pointer hover:translate-y-px active:translate-y-1 duration-100 font-semibold"
-              style={{ background: "#" + title.accent }}
+              style={{
+                background: "#" + item.accent,
+                boxShadow: `0px 0px 25px #${item.accent}80`,
+              }}
             >
-              <Link href={title.link}>{title.title}</Link>
+              <Link href={item.link}>{item.title}</Link>
             </button>
           ))}
         </div>
